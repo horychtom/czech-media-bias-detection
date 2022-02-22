1f86a784bdb5a82dc004954dc6a4f7f5ace7bc59
Tomáš Horych <horyctom@fel.cvut.cz>
2022-02-22 14:50:31 +0100

    training on wiki data

--- src/utils/myutils.py
+++ src/utils/myutils.py
@@ -1,13 +1,21 @@
 
 import torch
 import gc
+import matplotlib.pyplot as plt
+import itertools
+import numpy as np
+
 from datasets import load_metric
+
+
+def tokenize(tokenizer,data):
+    """ for mapping over dataset"""
+    return tokenizer(data['sentence'],truncation=True)
 
 def clean_memory():
     """cuda memory gets full while training transformers"""
     gc.collect()
     torch.cuda.empty_cache()
-
 
 
 def compute_metrics(model,device,testing_dataloader):
@@ -34,3 +42,37 @@ def compute_metrics(model,device,testing_dataloader):
         metric.add_batch(predictions=predictions, references=batch["labels"])
         
     return metric.compute(average='micro')
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """ convenient for plotting cm
+
+    Args:
+        cm (_type_): confusion matrix
+        classes : labels for classes
+        normalize (bool, optional):normalize to 1? Defaults to False.
+        title : Defaults to 'Confusion matrix'.
+        cmap (_type_, optional): Style .Defaults to plt.cm.Blues.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
